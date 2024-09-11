3a896dba66f0cc01e449ea306771e817fa086f60
DerFlip09 <97379854+DerFlip09@users.noreply.github.com>
2024-09-11 15:06:33 +0200

    new project architecture

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,23 +1,4 @@
-import json
-import requests
-
-
-API_KEY = "4EvovkZHfIDIkocKnLoO1Q==zLCf47tkEv0Ibdgb"
-API_URL = "https://api.api-ninjas.com/v1/animals"
-
-
-def load_json_data(file_path: str):
-    with open(file_path, "r") as handle:
-        return json.load(handle)
-
-
-def get_data_from_api_by_name(name):
-    api_query = API_URL + '?name={}'.format(name)
-    response = requests.get(api_query, headers={'X-Api-Key': API_KEY})
-    if response.status_code == requests.codes.ok:
-        return response.json()
-    else:
-        print("Error:", response.status_code, response.text)
+from data_fetcher import get_data_from_api_by_name
 
 
 def get_file_data(filename: str) -> str:
---
+++ data_fetcher.py
@@ -0,0 +1,20 @@
+import json
+import requests
+
+
+API_KEY = "4EvovkZHfIDIkocKnLoO1Q==zLCf47tkEv0Ibdgb"
+API_URL = "https://api.api-ninjas.com/v1/animals"
+
+
+def load_json_data(file_path: str):
+    with open(file_path, "r") as handle:
+        return json.load(handle)
+
+
+def get_data_from_api_by_name(name):
+    api_query = API_URL + '?name={}'.format(name)
+    response = requests.get(api_query, headers={'X-Api-Key': API_KEY})
+    if response.status_code == requests.codes.ok:
+        return response.json()
+    else:
+        print("Error:", response.status_code, response.text)
